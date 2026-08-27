3a39761f8e6158f29ec2d264ae25110cfb03d9bb
ybahgys <171437216+nuggettowin@users.noreply.github.com>
2026-08-27 17:16:42 +0800

    Remove banner

--- src/main/java/janet/Ui.java
+++ src/main/java/janet/Ui.java
@@ -2,22 +2,10 @@ package janet;
 
 public class Ui {
     private static final String GREETING = "Hi! I'm Janet, your friendly informational assistant. Let me know what you need!";
-    private static final String BANNER = """
-                  ⠀ ⠘⠒⠖⠲⠒⠖⠲⠒⠖⠲⠒⠖⠲⢤⣀⠀
-                ⠀⠀⠀⣀⣴⠦⠠⣤⠤⠤⠤⠤⠤⠤⠤⠤⠤⣤⠈⣆
-                ⠀⣰⡾⠋⠀⠀⠀⣻⠀⠀⠀⠀⣖⣳⠀⠀⠀⣽⠀⣸
-                ⣰⠏⠖⣀⠀⠀⠀⣯⠀⠀⠀⠀⠀⠀⠀⠀⣰⢃⡴⠃
-                ⡇⠀⠀⠈⣳⣄⢀⡽⢤⡤⢤⡤⣤⠤⠔⠚⠉⠁⠀⠀
-                ⢭⣀⣀⡀⢠⠎⠛⠀⠀⠀⡎⠱⡌⢢⠀⠀⠀⠀⠀⠀
-                ⣽⠀⠀⠀⣇⠀⠀⠀⠀⠀⠇⠀⠇⠸⠀⠀⠀⠀⠀⠀
-                ⢻⣦⡠⠐⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-                ⠀⠈⠳⣎⠙⠢⣄⠀⠀⢀⣤⠴⡶⠀⠀⠀⠀⠀⠀⠀
-                ⠀⠀⠀⠈⠓⠶⠤⣉⣹⣁⣀⣋⣧⠀⠀⠀⠀⠀⠀⠀
-            """;
     private static final String goodbye = "Bye! Take it sleazy!";
 
     public void showGreeting() {
-        System.out.printf("%s\n%s\n", Ui.BANNER, Ui.GREETING);
+        System.out.printf("%s\n%s\n", Ui.GREETING);
     }
 
     public void showGoodbye() {
