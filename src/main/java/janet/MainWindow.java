ead38ec181af9cb04eb7db94771dd6c1fa082237
ybahgys <171437216+nuggettowin@users.noreply.github.com>
2026-09-15 18:17:18 +0800

    Modify photos for accuracy

--- src/main/java/janet/MainWindow.java
+++ src/main/java/janet/MainWindow.java
@@ -26,8 +26,8 @@ public class MainWindow extends AnchorPane {
 
     private Janet janet;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image janetImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.jpg"));
+    private Image janetImage = new Image(this.getClass().getResourceAsStream("/images/janet.jpg"));
 
     @FXML
     public void initialize() {
